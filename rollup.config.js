ff52296a83bc882138c664b7fb16830e9b5b7b3c
Tony Brix <tony@brix.ninja>
2022-11-02 07:08:09 +0000

    chore: fix rollup warnings

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,21 @@
-module.exports = [
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+export default [
   {
     input: 'src/index.js',
     output: {
       name: 'markedBidi',
       file: 'lib/index.umd.js',
       format: 'umd'
-    }
+    },
+    plugins: [nodeResolve()]
   },
   {
     input: 'src/index.js',
     output: {
       file: 'lib/index.cjs',
       format: 'cjs'
-    }
+    },
+    external: ['github-slugger']
   }
 ];
