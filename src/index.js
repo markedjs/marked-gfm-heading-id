7cabb917bcd257afe355cbc9915ebd2322fee0b5
Tony Brix <tony@brix.ninja>
2022-11-11 08:17:40 +0000

    fix: fix html in heading

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ export function gfmHeadingId({ prefix = '' } = {}) {
   return {
     renderer: {
       heading(text, level, raw) {
+        raw = raw.toLowerCase().trim().replace(/<[!\/a-z].*?>/ig, '');
         return `<h${level} id="${prefix}${slugger.slug(raw)}">${text}</h${level}>\n`;
       }
     }
