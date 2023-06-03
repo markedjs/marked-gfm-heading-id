8d10ae7fab2a420ca17784d9e64d6cd7713b5511
krzysdz <krzysdz@users.noreply.github.com>
2023-06-03 06:55:13 +0200

    fix: Add TypeScript declaration file (#324)

---
+++ spec/index.test-d.ts
@@ -0,0 +1,10 @@
+import { marked } from "marked";
+import { gfmHeadingId } from '../src/index.js';
+
+const options = {
+	prefix: "my-prefix-",
+};
+
+marked.use(gfmHeadingId(options));
+
+marked("# heading");
---
+++ src/index.d.ts
@@ -0,0 +1,15 @@
+import type { marked } from "marked"
+
+/** Options for configuring marked-gfm-heading-id extension */
+interface GfmHeadingIdOptions {
+  /** A string to prepend to all ids. Empty by default. */
+  prefix?: string;
+}
+
+/**
+ * Add `id` attribute to headings (h1, h2, h3, etc) like GitHub.
+ *
+ * @param options Options for the extension
+ * @returns A {@link marked.MarkedExtension | MarkedExtension} to be passed to {@link marked.use | `marked.use()`}
+ */
+export function gfmHeadingId(options?: GfmHeadingIdOptions): marked.MarkedExtension;
