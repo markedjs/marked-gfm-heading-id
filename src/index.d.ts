a72b5deac633da27bfaf587a331cf6bc6250098e
Matthieu Riegler <kyro38@gmail.com>
2023-09-23 05:37:42 +0200

    feat: add `getHeadingList()` (#413)

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,4 @@
-import type { MarkedExtension, marked } from "marked"
+import type {MarkedExtension, marked} from 'marked';
 
 /** Options for configuring marked-gfm-heading-id extension */
 interface GfmHeadingIdOptions {
@@ -10,6 +10,25 @@ interface GfmHeadingIdOptions {
  * Add `id` attribute to headings (h1, h2, h3, etc) like GitHub.
  *
  * @param options Options for the extension
- * @returns A {@link marked.MarkedExtension | MarkedExtension} to be passed to {@link marked.use | `marked.use()`}
+ * @returns A {@link marked.MarkedExtension | MarkedExtension} to be passed
+ *     to {@link marked.use | `marked.use()`}
  */
 export function gfmHeadingId(options?: GfmHeadingIdOptions): MarkedExtension;
+
+/**
+ * Headings information, can be used to create table of content
+ */
+export interface HeadingData {
+  level: number;
+  text: string;
+  id: string;
+}
+
+/**
+ * Returns a list of headings with the ids as computed by gfmHeadingId
+ *
+ * @param tokens a lexer output
+ * @param options Options for the extension
+ * @returns A string formatted the same as what would {@link gfmHeadingId} do.
+ */
+export function getHeadingList(): HeadingData[];
--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
 import GithubSlugger from 'github-slugger';
 let slugger;
 
+let headings = [];
+
 export function gfmHeadingId({ prefix = '' } = {}) {
   return {
     headerIds: false, // prevent deprecation warning; remove this once headerIds option is removed
     hooks: {
       preprocess(src) {
+        headings = [];
         slugger = new GithubSlugger();
         return src;
       }
     },
     renderer: {
       heading(text, level, raw) {
-        raw = raw.toLowerCase().trim().replace(/<[!\/a-z].*?>/ig, '');
-        return `<h${level} id="${prefix}${slugger.slug(raw)}">${text}</h${level}>\n`;
+        raw = raw
+          .toLowerCase()
+          .trim()
+          .replace(/<[!\/a-z].*?>/gi, '');
+        const id = `${prefix}${slugger.slug(raw)}`;
+        const heading = { level, text, id };
+        headings.push(heading);
+
+        return `<h${level} id="${id}">${text}</h${level}>\n`;
       }
     }
   };
+}
+
+export function getHeadingList() {
+  return headings;
 }
