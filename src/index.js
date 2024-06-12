61e822984a798debd26c504d83545466fcef10e3
Tony Brix <tony@brix.ninja>
2024-06-12 01:04:43 -0600

    fix!: allow marked v13

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ let slugger = new GithubSlugger();
 
 let headings = [];
 
+// unescape from marked helpers
+const unescapeTest = /&(#(?:\d+)|(?:#x[0-9A-Fa-f]+)|(?:\w+));?/ig;
+export function unescape(html) {
+  // explicitly match decimal, hex, and named HTML entities
+  return html.replace(unescapeTest, (_, n) => {
+    n = n.toLowerCase();
+    if (n === 'colon') return ':';
+    if (n.charAt(0) === '#') {
+      return n.charAt(1) === 'x'
+        ? String.fromCharCode(parseInt(n.substring(2), 16))
+        : String.fromCharCode(+n.substring(1));
+    }
+    return '';
+  });
+}
+
 export function gfmHeadingId({ prefix = '', globalSlugs = false } = {}) {
   return {
     headerIds: false, // prevent deprecation warning; remove this once headerIds option is removed
@@ -14,12 +30,15 @@ export function gfmHeadingId({ prefix = '', globalSlugs = false } = {}) {
         return src;
       }
     },
+    useNewRenderer: true,
     renderer: {
-      heading(text, level, raw) {
-        raw = raw
+      heading({ tokens, depth }) {
+        const text = this.parser.parseInline(tokens);
+        const raw = unescape(this.parser.parseInline(tokens, this.parser.textRenderer))
           .toLowerCase()
           .trim()
           .replace(/<[!\/a-z].*?>/gi, '');
+        const level = depth;
         const id = `${prefix}${slugger.slug(raw)}`;
         const heading = { level, text, id };
         headings.push(heading);
